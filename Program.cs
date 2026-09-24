46576ad7f5d6fe022a1843ecf2bec3dd5240c55b
NAHUEL LOTO <loto_nahuel@hotmail.com>
2026-09-23 21:00:10 -0300

    NewArquitectura

--- Program.cs
+++ Program.cs
@@ -17,61 +17,81 @@ Computer computer = new Computer
 computer.Open();
 computer.Accept(new UpdateVisitor());
 
-while (true)
+// while (true)
+// {
+foreach (IHardware hardware in computer.Hardware)
 {
-    foreach (IHardware hardware in computer.Hardware)
+    hardware.Update();
+    Dictionary<string, List<(string nameHardware, string nameSensor, string valueSensor, string typeSensor)>> data_hardware = new();
+    if (
+        hardware.HardwareType == HardwareType.GpuNvidia ||
+        hardware.HardwareType == HardwareType.GpuAmd ||
+        hardware.HardwareType == HardwareType.GpuIntel ||
+        hardware.HardwareType == HardwareType.Cpu
+    )
     {
+        // Console.WriteLine($"\nHardware [{hardware.Name}]");
+        // Console.WriteLine($"{hardware.HardwareType}");
+        // // Console.WriteLine("Hardware: {0}", hardware.Name);
+
+        // foreach (IHardware subhardware in hardware.SubHardware)
+        // {
+        //     Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+            
+        //     foreach (ISensor sensor in subhardware.Sensors)
+        //     {
+        //         Console.WriteLine("\t-------------------------------------------------------------------------");
+        //         Console.WriteLine("\t\tSensor: {0}, value: {1}, is: {2}", sensor.Name, sensor.Value ?? 0, sensor.SensorType);
+        //     }
+        // }
+        
         hardware.Update();
-        // const salida = {$"Hardware: {hardware.Name}"};
-        if (
-            hardware.HardwareType == HardwareType.GpuNvidia ||
-            hardware.HardwareType == HardwareType.GpuAmd ||
-            hardware.HardwareType == HardwareType.GpuIntel ||
-            hardware.HardwareType == HardwareType.Cpu
-        )
+        
+        foreach (ISensor sensor in hardware.Sensors)
         {
-            Console.WriteLine($"\nHardware [{hardware.Name}]");
-            Console.WriteLine($"{hardware.HardwareType}");
-            // Console.WriteLine("Hardware: {0}", hardware.Name);
-
-            foreach (IHardware subhardware in hardware.SubHardware)
+            if (sensor.Value.HasValue)
             {
-                Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
-                
-                foreach (ISensor sensor in subhardware.Sensors)
+                string key = $"{hardware.HardwareType}";
+                string name_sensor = sensor.Name;
+                int i = 1;
+                while (data_hardware.ContainsKey(name_sensor))
                 {
-                    Console.WriteLine("\t-------------------------------------------------------------------------");
-                    Console.WriteLine("\t\tSensor: {0}, value: {1}, is: {2}", sensor.Name, sensor.Value ?? 0, sensor.SensorType);
+                    name_sensor = $"{sensor.Name} [{i}]";
+                    i ++;
                 }
+                if (!data_hardware.ContainsKey(key))
+                {
+                    data_hardware[key] = new List<(string, string, string, string)>();
+                }
+                data_hardware[key].Add(
+                    (
+                        $"{hardware.Name}",
+                        $"{sensor.Name}",
+                        $"{sensor.Value:F1}",
+                        $"{sensor.SensorType}"
+                    )
+                );
             }
-            foreach (ISensor sensor in hardware.Sensors)
+            
+        }
+        foreach (var key in data_hardware.Keys)
+        {
+            Console.Write($"\n{key}");
+            foreach (var sensor in data_hardware[key])
             {
-                hardware.Update();
-                if (sensor.SensorType == SensorType.Temperature)
-                {
-                    if (sensor.Value.HasValue)
-                    {
-                        string temperature = $"{sensor.Value:F1}";
-                        Console.WriteLine("\t-------------------------------------------------------------------------");
-                        Console.WriteLine($"\tSensor: {sensor.Name}\n\t\tValue: {temperature}\n\t\tType: {sensor.SensorType}");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("\t-------------------------------------------------------------------------");
-                        Console.WriteLine($"\tSensor: {sensor.Name}\n\t\tValue: NO HAY VALOR\n\t\tType: {sensor.SensorType}");
-                    }
-                }
+                Console.WriteLine($"\nHardware: {sensor.nameHardware}");
+                Console.WriteLine("\t-------------------------------------------------------------------------");
+                Console.WriteLine($"\tSensor: {sensor.nameSensor} \n\t\tValor: {sensor.valueSensor} \n\t\tType Sensor: {sensor.typeSensor}");
+                Console.WriteLine("\t-------------------------------------------------------------------------");
                 
             }
-            // break;
+            Console.WriteLine($"LARGO: {data_hardware[key].Count}");
         }
     }
-    Thread.Sleep(500);
 }
-
-
-computer.Close();
+    // Thread.Sleep(1000);
+    // computer.Close();
+// }
 
 public class UpdateVisitor : IVisitor
 {
